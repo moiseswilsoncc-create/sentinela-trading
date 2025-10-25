0edbaed174e304b40c02c1e8cd0ef164a3bfb739
moiseswilsoncc-create <moises.wilson.cc@gmail.com>
2025-10-25 11:43:18 -0300

    actualizado vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Configuración completa para Vercel
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    port: 5173
+  }
 })
